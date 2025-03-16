392a900efc74fe783af3bfd6f0e7f17d811d8353
Tejas Itankar <tejasitankar94820@gmail.com>
2025-03-17 01:08:19 +0530

    completed section-2 mobile and desktop view

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-import Link from "next/link";
+import section2List from '@/app/data/homepageSection2.json';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center ">
+
+      {/* section - 1 */}
       <section className="relative bg-[radial-gradient(circle_at_top_right,_#DCF5EC_0%,_transparent_50%)] h-[36rem] md:h-[38rem] lg:h-[40rem] w-full">
         <Image src="/homepage/section-upper-design.svg" className="absolute top-0 left-0 h-[10rem] md:h-[15rem] lg:h-[20rem] w-auto z-0" alt="section-design" height={500} width={500} />
-        
+
         <div className="flex flex-col-reverse md:flex-row justify-between h-full w-full absolute z-10 px-5 py-12 md:px-8 lg:px-10 md:py-34 ">
-          
+
           {/* left content */}
           <div className="flex flex-col gap-4 md:w-1/2">
             <div className="hidden md:flex flex-row gap-5 items-center">
@@ -31,6 +33,29 @@ export default function Home() {
           </div>
         </div>
         <Image src="/homepage/section-lower-design.svg" className="absolute bottom-0 right-0 h-[10rem] md:h-[15rem] lg:h-[20rem] w-auto z-0 " alt="section-design" height={500} width={500} />
+      </section>
+
+      {/* section - 2 */}
+
+      <section className="w-full h-fit flex flex-col items-center gap-5 px-5 md:px-8 lg:px-10 py-5 md:py-10">
+        <div className="flex flex-row items-center md:gap-10 lg:gap-20 ">
+          <Image src="/homepage/image-2.png" className="hidden md:block h-[30rem] lg:h-[38rem] w-auto" alt="bottle of tablets" height={500} width={500} />
+          <article className="">
+            <h2 className="font-bold text-2xl/10 md:text-4xl/10 ">India's Leading Hub for Pharmaceutical Excellence</h2>
+            {
+              section2List.map((item, index) => (
+                <div key={index} className="mt-8 md:w-5/6">
+                  <span className="flex flex-row items-center justify-start ">
+                    <Image src={item.icon} className="h-[1.5em] w-[1.5em]" alt="medal" height={10} width={10} />
+                    <span className="ml-1.5 font-bold text-base md:text-xl leading-normal">{item.title}</span>
+                  </span>
+                  <p className="mt-3 text-sm md:text-lg text-[#5F6980]">{item.description}</p>
+                </div>
+              ))
+            }
+          </article>
+        </div>
+        <button className="bg-[#3AA76D] text-white rounded-xl w-full lg:w-3/4 px-3 md:px-2 py-3 md:py-6 font-bold text-2xl md:text-4xl"> Trusted by 500+ clients</button>
       </section>
     </div>
   );
