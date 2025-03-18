05ff17b22e96aad0cfcfac62202adc6c8706bae3
Tejas Itankar <tejasitankar94820@gmail.com>
2025-03-18 20:04:15 +0530

    removed border from section-4

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -84,8 +84,8 @@ export default function Home() {
       </section>
 
       {/* section - 4 */}
-      <section className="relative w-full h-fit flex flex-row items-center gap-5 bg-[radial-gradient(circle_at_right,_#DCF5EC_0%,_transparent_50%)] px-5 md:px-8 lg:px-10 py-5 md:py-10 border">
-        <Image src="/homepage/section4Design.svg" className=" hidden md:block absolute h-[10rem] md:h-[15rem] lg:h-[20rem] w-[30rem] z-0 border border-black left-[-2.5rem] lg:left-0 bottom-1/5" alt="section-design" height={500} width={500} />
+      <section className="relative w-full h-fit flex flex-row items-center gap-5 bg-[radial-gradient(circle_at_right,_#DCF5EC_0%,_transparent_50%)] px-5 md:px-8 lg:px-10 py-5 md:py-10 ">
+        <Image src="/homepage/section4Design.svg" className=" hidden md:block absolute h-[10rem] md:h-[15rem] lg:h-[20rem] w-[30rem] z-0 left-[-2.5rem] lg:left-0 bottom-1/5" alt="section-design" height={500} width={500} />
         
         <div className="flex flex-col sm:items-center gap-6 md:gap-10 mx-auto z-10">
           <button className="border-2 border-[#509E49] text-[#509E49] rounded-full px-4 py-2 w-fit ">Our Milestones</button>
